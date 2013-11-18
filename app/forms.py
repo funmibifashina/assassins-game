aa0fb4fa574b1455061252a9a947a8581456e090
Samuel Taylor <github@samueltaylor.org>
2013-11-18 15:51:42 -0600

    registration skeleton

---
+++ app/forms.py
@@ -0,0 +1,11 @@
+from flask.ext.wtf import Form
+from wtforms import TextField, PasswordField
+from wtforms.validators import Required, Email, EqualTo
+
+class RegisterForm(Form):
+    username = TextField("Username", validators = [Required()])
+    password = PasswordField("Password", validators = [Required()])
+    passwordAgain = PasswordField("Confirm password",
+            validators = [Required(), EqualTo(password)])
+    email = TextField("Email", validators = [Email()])
+
--- app/views.py
+++ app/views.py
@@ -1,7 +1,13 @@
 from flask import render_template
 from app import app
+from forms import RegisterForm
 
 @app.route("/")
 def index():
     return render_template("index.html", title = "Title", message = "Message")
+
+@app.route("/register", methods = ["GET", "POST"])
+def register():
+    form = RegisterForm()
+    return render_template("register.html", title = "Register", form = form)
 
