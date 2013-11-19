622ea70c02116e0986a5aef89d7df44c40980bf7
Samuel Taylor <github@samueltaylor.org>
2013-11-18 21:12:14 -0600

    fixed validator error

--- app/forms.py
+++ app/forms.py
@@ -6,6 +6,6 @@ class RegisterForm(Form):
     username = TextField("Username", validators = [Required()])
     password = PasswordField("Password", validators = [Required()])
     passwordAgain = PasswordField("Confirm password",
-            validators = [Required(), EqualTo(password)])
+            validators = [Required(), EqualTo("password")])
     email = TextField("Email", validators = [Email()])
 
