7ac766829df16c0666b6978b2a20c266e5352afc
Samuel Taylor <github@samueltaylor.org>
2013-11-20 11:13:12 -0600

    detect duplicate emails

--- app/forms.py
+++ app/forms.py
@@ -1,13 +1,19 @@
 from flask.ext.wtf import Form
-from wtforms import TextField, PasswordField
+from wtforms import TextField, PasswordField, ValidationError
 from wtforms.validators import Required, Email, EqualTo
+from models import User
+
+def non_duplicate_email(form, field):
+    sameEmails = User.query.filter_by(email = field.data)
+    if len(sameEmails.all()) > 0:
+        raise ValidationError("Duplicate email detected")
 
 class RegisterForm(Form):
     username = TextField("Username", validators = [Required()])
     password = PasswordField("Password", validators = [Required()])
     passwordAgain = PasswordField("Confirm password",
             validators = [Required(), EqualTo("password")])
-    email = TextField("Email", validators = [Email()])
+    email = TextField("Email", validators = [Email(), non_duplicate_email])
 
 class LoginForm(Form):
     username = TextField("Username", validators = [Required()])
